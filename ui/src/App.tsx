ba1c7c58475106c1916632b73364392fd50df162
Andrew Sowinski <32100713+sowinskian@users.noreply.github.com>
2021-01-28 20:39:12 -0500

    this thing doesn't compile

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -48,7 +50,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit whyyyy<code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -60,6 +62,17 @@ function App() {
         </a>
         <p>{metaData.name} has coordinates: {metaData.latitude}, {metaData.longitude}.</p>
       </header>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[51.505, -0.09]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
     </div>
   );
 }
