7dbda53a8957f9335efcd70aef19380aace72344
Trevor Aten <traten11@gmail.com>
2021-02-03 12:35:44 -0500

    coord data of all stations returned as json

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import time
 import requests
+import json
+import array as arr 
 
 app = Flask(__name__)
 
@@ -30,3 +32,29 @@ def coords_test():
 
     res = requests.get(req)
     return res.json()
+
+
+@app.route('/getCoordsOfAllStations')
+def getCoordsOfAllStations():
+    server = 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter'
+    date = 'latest'
+    # station = '8727520'  # Cedar Key
+    product = 'water_level'
+    datum = 'STND'
+    units = 'english'
+    time_zone = 'gmt'
+    application = 'uf_tides'
+    res_format = 'json'
+
+    stationIds = open("Database/station_ids.txt", "r")
+    resDictList = []
+    
+    for x in stationIds:
+        print(x)
+        station = x
+        req = server + '?' + '&'.join(['date=' + date, 'station=' + station, 'product=' + product, 'datum=' + datum,
+                                    'units=' + units, 'time_zone=' + time_zone, 'application=' + application, 'format=' + res_format])
+        res = requests.get(req)
+        resJson = res.json()
+        resDictList.append(resJson)
+    return json.dumps(resDictList)
