fa7cdf23f23e295778783980a9ff6f8b44b94cef
Trevor Aten <traten11@gmail.com>
2021-03-18 17:48:29 -0400

    week view is not buggy anymore

--- ui/src/ChartComponent.tsx
+++ ui/src/ChartComponent.tsx
@@ -13,12 +13,12 @@ interface chartMetaData {
    labels: string[]
    resolution?: string
    normalorAverage?: string
-   level?: number[]
+   threshold?: number
  }
 
 export default function ChartComponent(props: chartMetaData) {
    let chartDataTemp: number[] = [];
-   let tempLabels = [];
+   let tempLabels: string[] = [];
    let unitConversion: number = 0;
 
    if(props.resolution === '6 mins') {
@@ -44,67 +44,104 @@ export default function ChartComponent(props: chartMetaData) {
          j++;
       }
    }
-   else if(props.resolution === '1 week') {
+   else if(props.resolution === '1 week' && props.normalorAverage === 'Normal') {
       unitConversion = sixMinsToWeek;
       let j = 0;
-      let length = props.chartData.length;
-      let alteredUnitConversion = unitConversion;
-      if(props.normalorAverage === 'Average') {
-         alteredUnitConversion = alteredUnitConversion - sixMinsToDay;
+      for(let i = 0; i < props.chartData.length; i = i + unitConversion) {
+            // console.log(props.labels.length);
+            // console.log(i);
+            chartDataTemp[j] = props.chartData[i];
+            tempLabels[j] = props.labels[i];
+            j++;
+         if(props.chartData.length - i <= unitConversion && props.chartData.length - i !== sixMinsToDay && props.chartData.length > sixMinsToWeek) {
+            i  = props.chartData.length - sixMinsToDay;
+            chartDataTemp[j] = props.chartData[i];
+            tempLabels[j] = props.labels[i];
+            i = props.chartData.length;
+            j++;
+         }
       }
-      for(let i = 0; i < props.chartData.length; i = i + alteredUnitConversion){
-         chartDataTemp[j] = props.chartData[i]; 
-         tempLabels[j] = props.labels[i];
-         j++;
-         // console.log("here");
-         // console.log(length)
-         // if(length < unitConversion && length > 0) {
-         //    i = i + length - sixMinsToDay;
-         //    length = 0;
-         //    console.log(length);
-         // }
-         // else {
-         //    i = i + unitConversion;
-         //    length = length - unitConversion;
-         // }
+   }
+   else if(props.resolution === '1 week' && props.normalorAverage === 'Average') {
+      unitConversion = sixMinsToWeek;
+      let sum: number = 0;
+      let average: number = 0;
+      let j = 0;
+      let count = 0;
+      let newStartDate: string = '';
+      let numOfDays = props.chartData.length/sixMinsToDay;
+      console.log(numOfDays);
+      for(let i = 0; i < props.chartData.length; i++) {
+         count++;
+         sum = sum + +props.chartData[i];
+         if(i % unitConversion === 0 && i !== 0) {
+            console.log("mod");
+            console.log(props.chartData.length);
+            console.log(i);
+            average = sum/count;
+            chartDataTemp[j] = +average.toFixed(3);
+            sum = 0;
+            count = 0;
+            average = 0;
+            if(props.labels[i - sixMinsToWeek] !== undefined && props.labels[i - 1] !== undefined && props.labels[i]) {
+               tempLabels[j] = props.labels[i - sixMinsToWeek].split(" ")[0] + '--' + props.labels[i - 1].split(" ")[0];
+               newStartDate = props.labels[i].split(" ")[0];
+            }
+            j++;
+            numOfDays -= 7;
+         }
+         else if(numOfDays <= 7) {
+            console.log("else");
+            console.log(props.chartData.length);
+            console.log(i);
+            while(i < props.chartData.length - 1) {
+               i++;
+               sum = sum + +props.chartData[i];
+               count++;
+            }
+            average = sum/count;
+            console.log(+average.toFixed(3));
+            chartDataTemp[j] = +average.toFixed(3);
+            sum = 0;
+            count = 0;
+            average = 0;
+            if(tempLabels[j-1] !== undefined && props.labels[i - 1] !== undefined) {
+               tempLabels[j] = newStartDate + '--' + props.labels[i - 1].split(" ")[0];
+            }
+            console.log(i);
+            console.log(props.labels[0]);
+            console.log(props.labels[i - 1]);
+            if(props.chartData.length <= sixMinsToWeek && props.labels[0] !== undefined && props.labels[i - 1] !== undefined) {
+               tempLabels[j] = props.labels[0].split(" ")[0] + '---' + props.labels[i - 1].split(" ")[0];
+            }
+            i = props.chartData.length;
+            j++;
+         }
       }
+      
    }
 
    if(props.normalorAverage === 'Normal' || props.resolution === '6 mins') {
 
    }
-   else if(props.normalorAverage === 'Average' && props.resolution !== '6 mins') {
+   else if(props.normalorAverage === 'Average' && props.resolution !== '6 mins' && props.resolution !== '1 week') {
       let average: number = 0;
       let sum: number = 0;
       let j = 0;
       for(let i = 0; i < props.chartData.length; ) {
          let k = 0;
          for(; (k < unitConversion && (k+i) < props.chartData.length); k++){
-            // console.log(+props.chartData[k+i]);
             sum = sum + +props.chartData[k + i];
          }
          i = i + k;
-         // console.log(k);
-         // console.log(sum);
          average = sum/k;
-         // console.log(average);
          chartDataTemp[j] = +average.toFixed(3);
-         if(unitConversion === sixMinsToWeek) {
-            if(tempLabels[j+1] !== undefined) {
-               tempLabels[j] = tempLabels[j].split(" ")[0] + '-' + tempLabels[j + 1].split(" ")[0]
-            }
-         }
-         else {
-            if(tempLabels[j] !== undefined) {
-               tempLabels[j] = tempLabels[j].split(" ")[0];
-            }
+         if(tempLabels[j] !== undefined) {
+            tempLabels[j] = tempLabels[j].split(" ")[0];
          }
          sum = 0;
          average = 0;
          j++;
-      }
-      if(unitConversion === sixMinsToWeek) {
-         tempLabels.pop();
       }
    }
 
